4d4d9ad424b6c2c006de2399a93ae1f2dbaa2c1f
Darren Hobbs <darrenhobbs@mac.com>
2016-02-10 23:36:43 +0000

    Openshift

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(8080, function(){
-    console.log('listening on *:8080');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+
+http.listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
 });
